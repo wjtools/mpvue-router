8afacdd1bfadd9ad4f2780de2747c07875726680
steven <wangwen1220@139.com>
2019-01-04 11:33:24 +0800

    'init'

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # mpvue-router
-在 mpvue 中使用类 vue-router 的路由写法
+> 在 mpvue 中使用类 vue-router 的路由写法
+> 基于 [mpvue-router-patch](https://github.com/F-loat/mpvue-router-patch#readme)，感谢原作者！
+> 修复返回上一页后 $route.query 取值为空的问题，以及一些定制功能
+
+## 安装
+
+``` bash
+npm i mpvue-router
+```
+
+## 使用
+
+``` js
+// main.js
+import Vue from 'vue'
+import MpvueRouter from 'mpvue-router'
+
+Vue.use(MpvueRouter)
+```
+
+## API
+
+> 用法与 [mpvue-router-patch](https://github.com/F-loat/mpvue-router-patch#readme) 相同
---
+++ src/index.js
@@ -0,0 +1,97 @@
+import { stringify } from './querystring'
+
+function parseUrl(location) {
+  if (typeof location === 'string') return location
+
+  const { path, query } = location
+  const queryStr = stringify(query)
+
+  if (!queryStr) {
+    return path
+  }
+
+  return `${path}?${queryStr}`
+}
+
+function parseRoute($mp) {
+  const _$mp = $mp || {}
+  const path = _$mp.page && _$mp.page.route
+  const query = _$mp.page && _$mp.page.options
+  return {
+    path: `/${path}`,
+    params: {},
+    query,
+    hash: '',
+    fullPath: parseUrl({
+      path: `/${path}`,
+      query
+    }),
+    name: path && path.replace(/\/(\w)/g, ($0, $1) => $1.toUpperCase())
+  }
+}
+
+function push(location, complete, fail, success) {
+  const url = parseUrl(location)
+  const params = { url, complete, fail, success }
+
+  if (location.isTab) {
+    wx.switchTab(params)
+    return
+  }
+  if (location.reLaunch) {
+    wx.reLaunch(params)
+    return
+  }
+  wx.navigateTo(params)
+}
+
+function replace(location, complete, fail, success) {
+  const url = parseUrl(location)
+  wx.redirectTo({ url, complete, fail, success })
+}
+
+function go(delta) {
+  wx.navigateBack({ delta })
+}
+
+function back() {
+  wx.navigateBack()
+}
+
+export let _Vue
+
+export default {
+  install(Vue) {
+    if (this.installed && _Vue === Vue) return
+    this.installed = true
+
+    _Vue = Vue
+
+    const _router = {
+      mode: 'history',
+      push,
+      replace,
+      go,
+      back
+    }
+
+    Vue.mixin({
+      onLoad() {
+        const { $mp } = this.$root
+        this._route = parseRoute($mp)
+      },
+      onShow() {
+        _router.app = this
+        _router.currentRoute = this._route
+      }
+    })
+
+    Object.defineProperty(Vue.prototype, '$router', {
+      get() { return _router }
+    })
+
+    Object.defineProperty(Vue.prototype, '$route', {
+      get() { return this._route }
+    })
+  }
+}
